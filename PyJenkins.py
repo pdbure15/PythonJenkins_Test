36cd4b80814abf6b93b692e0d754b8a96d2fba56
pdbure15 <pbure@stevens.edu>
2017-07-14 14:58:41 -0700

    Jenkins build must start after commit

--- PyJenkins.py
+++ PyJenkins.py
@@ -4,3 +4,5 @@ print 'This is a Python program execution through Jenkins!!!'
 print 'New statement to check build using Jenkins!!!'
 
 print 'Got it working finally!!!'
+
+print 'Jenkins job will run once git commit is pushed'
